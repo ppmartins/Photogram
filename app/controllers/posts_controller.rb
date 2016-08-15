05d889ba3ab14ae0cbdfab890657125a53ac50f0
Pedro <ppereiramartins@gmail.com>
2016-08-15 18:58:28 +0100

    Work in progress

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to posts_path
+    else
+      flash[:alert] = "Halt, you fiend! You need an image to post here!"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  if @post.update(post_params)
+    flash[:success] = "Post updated."
+    redirect_to root_path
+  else
+    flash[:alert] = "Something is wrong with your form!"
+    render :edit
+  end
+end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
---
+++ spec/features/editing_posts_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'Editing posts' do
+  before do   #it should be "background, instead of before"
+    job = create(:post)
+
+    visit '/'
+    find(:xpath, "//a[contains(@href,'posts/1')]").click
+    click_link 'Edit Post'
+  end
+
+  scenario 'Can edit a post' do
+    fill_in 'Caption', with: "Oh god, you weren't meant to see this picture!"
+    click_button 'Update Post'
+
+    expect(page).to have_content("Post updated.")
+    expect(page).to have_content("Oh god, you weren't meant to see this picture!")
+  end
+
+  scenario "won't update a post without an image" do
+    attach_file('Image', 'spec/files/images/coffee.jpg')
+    click_button 'Update Post'
+
+    expect(page).to have_content("Something is wrong with your form!")
+  end
+end
